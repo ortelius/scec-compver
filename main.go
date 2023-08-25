ba71c8e57608fd4300beb19d040d966c8057566a
Steve Taylor <steve@deployhub.com>
2023-08-25 10:24:43 -0600

    add filter to list query Signed-off-by: Steve Taylor <steve@deployhub.com>

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
-// Ortelius v11 Domain Microservice that handles creating and retrieving Domains
+// Ortelius v11 CompVer Microservice that handles creating and retrieving CompVer
 package main
 
 import (
 	"context"
+	"encoding/json"
 
 	_ "cli/docs"
 
@@ -16,23 +17,24 @@ import (
 var logger = database.InitLogger()
 var dbconn = database.InitializeDB()
 
-// GetDomains godoc
-// @Summary Get a List of Domains
-// @Description Get a list of domains for the user.
-// @Tags domain
+// GetComponentVersions godoc
+// @Summary Get a List of Component Versions
+// @Description Get a list of component versions for the user.
+// @Tags compver
 // @Accept */*
 // @Produce json
 // @Success 200
-// @Router /msapi/domain [get]
-func GetDomains(c *fiber.Ctx) error {
+// @Router /msapi/compver [get]
+func GetComponentVersions(c *fiber.Ctx) error {
 
 	var cursor driver.Cursor       // db cursor for rows
 	var err error                  // for error handling
 	var ctx = context.Background() // use default database context
 
-	// query all the domains in the collection
-	aql := `FOR domain in evidence
-			RETURN domain`
+	// query all the compvers in the collection
+	aql := `FOR compver in evidence
+			FILTER (compver.objtype == 'ComponentVersionDetails')
+			RETURN compver`
 
 	// execute the query with no parameters
 	if cursor, err = dbconn.Database.Query(ctx, aql, nil); err != nil {
@@ -41,33 +43,33 @@ func GetDomains(c *fiber.Ctx) error {
 
 	defer cursor.Close() // close the cursor when returning from this function
 
-	var domains []model.Domain // define a list of domains to be returned
+	components := model.NewComponents() // define a list of compvers to be returned
 
 	for cursor.HasMore() { // loop thru all of the documents
 
-		var domain model.Domain      // fetched domain
-		var meta driver.DocumentMeta // data about the fetch
+		compver := model.NewComponentVersion() // fetched compver
+		var meta driver.DocumentMeta           // data about the fetch
 
 		// fetch a document from the cursor
-		if meta, err = cursor.ReadDocument(ctx, &domain); err != nil {
+		if meta, err = cursor.ReadDocument(ctx, compver); err != nil {
 			logger.Sugar().Errorf("Failed to read document: %v", err)
 		}
-		domains = append(domains, domain)                                    // add the domain to the list
+		components.Components = append(components.Components, compver)       // add the compver to the list
 		logger.Sugar().Infof("Got doc with key '%s' from query\n", meta.Key) // log the key
 	}
 
-	return c.JSON(domains) // return the list of domains in JSON format
+	return c.JSON(components) // return the list of compvers in JSON format
 }
 
-// GetDomain godoc
-// @Summary Get a Domain
-// @Description Get a domain based on the _key or name.
-// @Tags domain
+// GetComponentVersionDetails godoc
+// @Summary Get a Component Version
+// @Description Get a compver details based on the _key or name.
+// @Tags compver
 // @Accept */*
 // @Produce json
 // @Success 200
-// @Router /msapi/domain/:key [get]
-func GetDomain(c *fiber.Ctx) error {
+// @Router /msapi/compver/:key [get]
+func GetComponentVersionDetails(c *fiber.Ctx) error {
 
 	var cursor driver.Cursor       // db cursor for rows
 	var err error                  // for error handling
@@ -78,10 +80,10 @@ func GetDomain(c *fiber.Ctx) error {
 		"key": key,
 	}
 
-	// query the domains that match the key or name
-	aql := `FOR domain in books
-			FILTER (domain.name == @key or domain._key == @key)
-			RETURN domain`
+	// query the compvers that match the key or name
+	aql := `FOR compver in evidence
+			FILTER (compver.name == @key or compver._key == @key)
+			RETURN compver`
 
 	// run the query with patameters
 	if cursor, err = dbconn.Database.Query(ctx, aql, parameters); err != nil {
@@ -90,84 +92,81 @@ func GetDomain(c *fiber.Ctx) error {
 
 	defer cursor.Close() // close the cursor when returning from this function
 
-	var domain model.Domain // define a domain to be returned
+	compver := model.NewComponentVersionDetails() // define a compver to be returned
 
-	if cursor.HasMore() { // domain found
+	if cursor.HasMore() { // compver found
 		var meta driver.DocumentMeta // data about the fetch
 
-		if meta, err = cursor.ReadDocument(ctx, &domain); err != nil {
+		if meta, err = cursor.ReadDocument(ctx, compver); err != nil { // fetch the document into the object
 			logger.Sugar().Errorf("Failed to read document: %v", err)
 		}
 		logger.Sugar().Infof("Got doc with key '%s' from query\n", meta.Key)
 
 	} else { // not found so get from NFT Storage
-		key, cid2json := database.FetchFromLTS(key) // Use the CID to get the JSON from nft.storage
-		domain.Key = key                            // set the key for unmarshaling
-		domain.UnmarshalNFT(cid2json)               // convert the JSON to the domain object
+		if jsonStr, exists := database.MakeJSON(key); exists {
+			json.Unmarshal([]byte(jsonStr), compver) // convert the JSON string from LTF into the object
+		}
 	}
 
-	return c.JSON(domain) // return the domain in JSON format
+	return c.JSON(compver) // return the compver in JSON format
 }
 
-// NewDomain godoc
-// @Summary Create a Domain
-// @Description Create a new Domain and persist it
-// @Tags domain
+// NewComponentVersionDetails godoc
+// @Summary Create a CompVer
+// @Description Create a new CompVer and persist it
+// @Tags compver
 // @Accept application/json
 // @Produce json
 // @Success 200
-// @Router /msapi/domain [post]
-func NewDomain(c *fiber.Ctx) error {
+// @Router /msapi/compver [post]
+func NewComponentVersionDetails(c *fiber.Ctx) error {
 
-	var err error                  // for error handling
-	var meta driver.DocumentMeta   // data about the document
-	var ctx = context.Background() // use default database context
-	domain := new(model.Domain)    // define a domain to be returned
+	var err error                                 // for error handling
+	var meta driver.DocumentMeta                  // data about the document
+	var ctx = context.Background()                // use default database context
+	compver := model.NewComponentVersionDetails() // define a compver to be returned
 
-	if err = c.BodyParser(domain); err != nil { // parse the JSON into the domain object
+	if err = c.BodyParser(compver); err != nil { // parse the JSON into the compver object
 		return c.Status(503).Send([]byte(err.Error()))
 	}
 
-	cid2json := make(map[string]string, 0) // create a map for normalizing the domain object into CIDs + JSON strings
-	nft := domain.MarshalNFT(cid2json)     // convert the domain object into a JSON for nft.storage
+	cid, dbStr := database.MakeNFT(compver) // normalize the object into NFTs and JSON string for db persistance
 
-	logger.Sugar().Infof("%+v\n", nft) // log the new nft
+	logger.Sugar().Infof("%s=%s\n", cid, dbStr) // log the new nft
 
-	// add the domain to the database.  Ignore if it already exists since it will be identical
-	if meta, err = dbconn.Collection.CreateDocument(ctx, domain); err != nil && !driver.IsConflict(err) {
+	// add the compver to the database.  Ignore if it already exists since it will be identical
+	if meta, err = dbconn.Collection.CreateDocument(ctx, compver); err != nil && !driver.IsConflict(err) {
 		logger.Sugar().Errorf("Failed to create document: %v", err)
 	}
 	logger.Sugar().Infof("Created document in collection '%s' in db '%s' key='%s'\n", dbconn.Collection.Name(), dbconn.Database.Name(), meta.Key)
 
-	database.PersistOnLTS(cid2json) // save the nft JSON version of the domain object to ntf.storage
-
-	return c.JSON(domain) // return the domain object in JSON format.  This includes the new _key
+	return c.JSON(compver) // return the compver object in JSON format.  This includes the new _key
 }
 
 // setupRoutes defines maps the routes to the functions
 func setupRoutes(app *fiber.App) {
 
-	app.Get("/swagger/*", swagger.HandlerDefault) // handle displaying the swagger
-	app.Get("/msapi/domain", GetDomains)          // list of domains
-	app.Get("/msapi/domain/:key", GetDomain)      // single domain based on name or key
-	app.Post("/msapi/domain", NewDomain)          // save a single domain
+	app.Get("/swagger/*", swagger.HandlerDefault)              // handle displaying the swagger
+	app.Get("/msapi/compver", GetComponentVersions)            // list of compvers
+	app.Get("/msapi/compver/:key", GetComponentVersionDetails) // single compver based on name or key
+	app.Post("/msapi/compver", NewComponentVersionDetails)     // save a single compver
 }
 
-// @title Ortelius v11 Domain Microservice
+// @title Ortelius v11 CompVer Microservice
 // @version 11.0.0
-// @description RestAPI for the Domain Object
+// @description RestAPI for the CompVer Object
 // @termsOfService http://swagger.io/terms/
 // @contact.name Ortelius Google Group
 // @contact.email ortelius-dev@googlegroups.com
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 // @host localhost:3000
-// @BasePath /msapi/domain
+// @BasePath /msapi/compver
 func main() {
-	port := ":" + database.GetEnvDefault("MS_POST", "8080")
-	app := fiber.New()                       // create a new fiber application
-	setupRoutes(app)                         // define the routes for this microservice
-	if err := app.Listen(port); err != nil { // start listening for incoming connections
+	port := ":" + database.GetEnvDefault("MS_PORT", "8080") // database port
+	app := fiber.New()                                      // create a new fiber application
+	setupRoutes(app)                                        // define the routes for this microservice
+	if err := app.Listen(port); err != nil {                // start listening for incoming connections
 		logger.Sugar().Fatalf("Failed get the microservice running: %v", err)
 	}
 }
